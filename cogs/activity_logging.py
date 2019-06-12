ced4d5818f2b3575eb266dcbded79acd57deeb26
Cradac <39528837+Cradac@users.noreply.github.com>
2019-06-12 17:09:59 +0200

    might be a good one-liner

--- cogs/activity_logging.py
+++ cogs/activity_logging.py
@@ -81,7 +81,7 @@ class Activity_Logging(commands.Cog):
         for member in list_to_check:
             if not member.bot:
                 m_info = self.Storage.get_user_activity(member)
-                comparedate = m_info['timestamp'] or member.joined_at
+                comparedate = m_info['timestamp'] or (member.joined_at if (datetime.utcnow() - member.joined_at).days <=30 else 30)
                 m_info['days_gone'] = (datetime.utcnow() - comparedate).days
                 if m_info['days_gone'] > compare_days:
                     info_list.append(m_info)
