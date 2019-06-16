6e59a4e043a0da3a0390be6b46769de68d73d7af
Cradac <39528837+Cradac@users.noreply.github.com>
2019-06-16 15:17:53 +0200

    removed cursor buffer

--- utils/storage.py
+++ utils/storage.py
@@ -19,7 +19,7 @@ class Storage:
     
     def get_cursor(self):
         try:
-            cursor = self.conn.cursor(buffered=True)
+            cursor = self.conn.cursor()
             return cursor
         except Exception as e:
             raise e
